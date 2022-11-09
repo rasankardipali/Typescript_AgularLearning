e2f214927bf11e30a2b347cb7c4076116e4bd47b
rasankardipali <rasankardipali@gmail.com>
2022-11-09 15:37:53 +0530

    typescript basic
    typescript basic

---
+++ variable.js
@@ -0,0 +1,17 @@
+var no = 10;
+console.log(no);
+var name1 = "RASANKAR DIPALI HEMANT";
+console.log(name1);
+var num = 120; //new created variable
+console.log(num);
+var a1 = {
+    productID: 1,
+    ProductName: 'iPhone',
+    productPrice: 25000
+};
+console.log(a1);
+// homodenous array 
+var arr1 = ['Angular', 'React', 'NodeJS', 'java'];
+// heterogenous array 
+var arr2 = ['Angular', 123456, true];
+console.log(arr2);
---
+++ variable.ts
@@ -0,0 +1,27 @@
+var no: number =10;
+console.log(no);
+var name1 :String ="RASANKAR DIPALI HEMANT";
+console.log(name1);
+
+
+
+
+var num:number=120; //new created variable
+ console.log(num);
+ 
+var a1: any = {
+    productID: 1,
+    ProductName: 'iPhone',
+    productPrice: 25000
+}
+
+console.log(a1);
+
+// homodenous array 
+var arr1 : Array<string> = ['Angular', 'React', 'NodeJS', 'java'];
+
+// heterogenous array 
+var arr2 : Array<any> = ['Angular', 123456, true];
+
+
+console.log(arr2);
